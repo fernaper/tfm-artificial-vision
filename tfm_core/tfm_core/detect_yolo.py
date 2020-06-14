63e40b2e928b3752a2350a14f3b3df487513fd3d
Fernando <fernaperg@gmail.com>
2020-06-14 14:28:58 +0200

    :sparkles: Now can measure performance from YOLO, also now it uses GPU

--- tfm_core/tfm_core/detect_yolo.py
+++ tfm_core/tfm_core/detect_yolo.py
@@ -27,6 +27,9 @@ class Yolo(Dense_OF):
 
         self.labels = labels
         self.colors = np.random.randint(0, 255, size=(len(self.labels), 3),dtype="uint8")
+        self.measure_performance = kwargs.get('measure_performance', False)
+        self.draw_frame = kwargs.get('draw_frame', True)
+        print(self.measure_performance)
 
 
     def run(self):
@@ -37,28 +40,36 @@ class Yolo(Dense_OF):
             if self.__scale != 1:
                 frame = cv2.resize(frame, None, fx=self.__scale, fy=self.__scale)
 
-            gray_frame, hsv, end = self.next_frame(frame, gray_frame, hsv, show=True)
+            gray_frame, hsv, end = self.next_frame(frame, gray_frame, hsv, show=self.draw_frame)
 
             if end:
                 break
 
+        if self.measure_performance:
+            print(self.manager_cv2.get_fps())
 
-    def next_frame(self, frame, gray_frame, hsv, show=False):
-        gray_frame, hsv, _ = super().next_frame(frame, gray_frame, hsv)
 
-        thresholding_frame = self.threshold_frame(cv2.split(hsv)[2]) # I am sending the gray layer
-        detection_frame = self.detect(thresholding_frame, frame)
+    def next_frame(self, frame, gray_frame, hsv, show=False, dense=False):
+        gray_frame, hsv = None, None
+
+        if dense:
+            gray_frame, hsv, _ = super().next_frame(frame, gray_frame, hsv)
+            thresholding_frame = self.threshold_frame(cv2.split(hsv)[2])
+
+        detection_frame = self.detect(frame)
 
         end = False
         if show:
-            bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-            for region_from, region_to, _ in self.get_regions(thresholding_frame, frame):
-                cv2.rectangle(frame, region_from, region_to, (20,0,150), 2)
+            if dense:
+                bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+                for region_from, region_to, _ in self.get_regions(thresholding_frame, frame):
+                    cv2.rectangle(frame, region_from, region_to, (20,0,150), 2)
 
-            cv2.imshow('BGR', bgr)
-            cv2.imshow('Thresholding frame', thresholding_frame)
-            cv2.imshow('Dense clasifier', detection_frame)
-            cv2.imshow('Regions from contours', frame)
+                cv2.imshow('BGR', bgr)
+                cv2.imshow('Thresholding frame', thresholding_frame)
+                cv2.imshow('Regions from contours', frame)
+
+            cv2.imshow('YOLO detector', detection_frame)
             
             if cv2.waitKey(1) == ord('q'):
 	            end = True
@@ -83,15 +94,9 @@ class Yolo(Dense_OF):
             yield (x, y), (x+w, y+h), frame_to_crop[y:y+h, x:x+w]
 
 
-    def detect(self, thresholding_frame, frame):
+    def detect(self, frame):
         frame = frame.copy()
         H, W = frame.shape[:2]
-
-        '''
-        regions = self.get_regions(thresholding_frame, frame)
-
-        if not regions:
-            return frame'''
 
         blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
         self.net.setInput(blob)
@@ -180,9 +185,15 @@ if __name__ == "__main__":
         help='Scale of the video (default 1.0)',
         type=float)
     
-    default_yolo_path = os.path.sep.join([config.PROJECT_PATH, 'tfm-extra'])
+    default_yolo_path = os.path.join(config.PROJECT_PATH, 'tfm-extra')
     parser.add_argument("-y", "--yolo", default=default_yolo_path,
 	    help="Base path to YOLO directory")
+
+    parser.add_argument('-n', '--no_interface', action='store_false',
+                        help='Show interface (True|False)')
+
+    parser.add_argument('-m', '--measure_performance', action='store_true',
+                        help='Measure performance (True|False)')
 
     args = parser.parse_args()
 
@@ -193,6 +204,8 @@ if __name__ == "__main__":
     # and determine only the *output* layer names that we need from YOLO
     print("[INFO] loading YOLO from disk...")
     net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
     labelsPath = os.path.sep.join([args.yolo, "coco.names"])
     labels = open(labelsPath).read().strip().split("\n")
@@ -201,6 +214,11 @@ if __name__ == "__main__":
 
     if args.scale is not None:
         kwargs['scale'] = args.scale
+
+    if not args.no_interface:
+        kwargs['draw_frame'] = False
+
+    kwargs['measure_performance'] = args.measure_performance
 
     dc = Yolo(net, labels, args.video, args.stream, args.fps, **kwargs)
     dc.run()
